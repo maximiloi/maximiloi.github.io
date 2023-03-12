50739985d45035e019c91fd21861b14a138e9a5c
maximiloi <maximiloi@gmail.com>
2023-03-13 00:01:45 +0300

    time test (Mon, Mar 13, 2023 12:01:45 AM)

--- js/100_Days/day_18-Budget_App/script.js
+++ js/100_Days/day_18-Budget_App/script.js
@@ -98,4 +98,6 @@ productButton.addEventListener('click', () => {
     nameProductInput.value = '';
     costProductInput.value = '';
 });
+
+// git commit -m "feat:... -`LANG=en_EN date +'(%a, %b %d, %Y %r)'`-"
 //"<massage>date +'%Y-%m-%d %H:%M:%S'"
